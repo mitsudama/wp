14424a84fa459bf310689cb145ee1fe2d1feff52
mitsudama <admin@mitsudama.jp>
2017-08-08 02:22:35 +0900

    Add social buttons for twentyseventeen theme

--- wp/wp-content/themes/twentyseventeen/single.php
+++ wp/wp-content/themes/twentyseventeen/single.php
@@ -22,6 +22,20 @@ get_header(); ?>
 
 				get_template_part( 'template-parts/post/content', get_post_format() );
 
+$url = get_permalink();
+print <<<HTMLBLOCK
+<div id="fb-root" style="margin-bottom:30px;"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.10";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-like" data-href="$url" data-layout="standard" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>
+<a href="https://twitter.com/share" class="twitter-share-button" data-via="mitsudama_jp">Tweet</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+HTMLBLOCK;
+
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
